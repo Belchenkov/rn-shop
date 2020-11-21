7df8262813369a8c5f1bd3e338476fc59c7665b5
Aleksey Belchenkov <u608110@gmail.com>
2020-11-21 19:39:54 +0300

    User Products Screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -10,6 +10,7 @@ import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import Colors from "../constants/Colors";
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
+import UserProductsScreen from "../screens/user/UserProductsScreen";
 
 const defaultNavOptions = {
     headerStyle: {
@@ -59,9 +60,25 @@ const OrdersNavigator = createStackNavigator({
     }
 });
 
+const AdminNavigator = createStackNavigator({
+    UserProducts: UserProductsScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+            <Ionicons
+                name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                size={23}
+                color={drawerConfig.tintColor}
+            />
+        )
+    },
+    defaultNavigationOptions: defaultNavOptions
+});
+
 const ShopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
